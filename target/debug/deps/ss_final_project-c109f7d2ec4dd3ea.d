4863c7e0ea2b269e2127946cae515b786d790182
Jim Slusser <jslusser@kurkuma.co>
2022-09-26 16:12:47 -0700

    VSCode

--- target/debug/deps/ss_final_project-c109f7d2ec4dd3ea.d
+++
@@ -1,5 +0,0 @@
-/Users/jamesslusser/ss-final-project/target/debug/deps/ss_final_project-c109f7d2ec4dd3ea.rmeta: src/main.rs
-
-/Users/jamesslusser/ss-final-project/target/debug/deps/ss_final_project-c109f7d2ec4dd3ea.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/ss_final_project-c1198b5f38e63c65.d
+++
@@ -1,5 +0,0 @@
-/Users/jamesslusser/ss-final-project/target/debug/deps/ss_final_project-c1198b5f38e63c65.rmeta: src/main.rs
-
-/Users/jamesslusser/ss-final-project/target/debug/deps/ss_final_project-c1198b5f38e63c65.d: src/main.rs
-
-src/main.rs:
